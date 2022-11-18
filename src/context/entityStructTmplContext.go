9b377194f7a18fc4cdef1ea21d64f6cea9ba8214
jansen <liasece@gmail.com>
2022-11-18 15:53:27 +0800

    add HasFieldsWithTagName

--- src/context/entityStructTmplContext.go
+++ src/context/entityStructTmplContext.go
@@ -1,6 +1,8 @@
 package context
 
 import (
+	"fmt"
+
 	"github.com/liasece/go-mate/src/gogen/writer/repo"
 	"github.com/liasece/go-mate/src/utils"
 	"github.com/liasece/gocoder"
@@ -51,4 +53,14 @@ func (e *EntityStructTmplContext) HasFieldsWithTag2(tagReg string, tagReg2 strin
 // tagReg like: `gomate:url` match: `gomate:"foo,url"`
 func (e *EntityStructTmplContext) HasFieldsWithTag3(tagReg string, tagReg2 string, tagReg3 string) bool {
 	return len(e.FieldsWithTag(tagReg)) > 0 || len(e.FieldsWithTag(tagReg2)) > 0 || len(e.FieldsWithTag(tagReg3)) > 0
+}
+
+// tagReg like: `gomate:url` match: `gomate:"foo,url"`
+func (e *EntityStructTmplContext) HasFieldsWithTagName(tagName string, l1Name string) bool {
+	return len(e.FieldsWithTag(fmt.Sprintf("%s:%s", tagName, l1Name))) > 0
+}
+
+// tagReg like: `gomate:url` match: `gomate:"foo,url"`
+func (e *EntityStructTmplContext) HasFieldsWithTagNameL2(tagName string, l1Name string, l2Name string) bool {
+	return len(e.FieldsWithTag(fmt.Sprintf("%s:%s:%s", tagName, l1Name, l2Name))) > 0
 }
