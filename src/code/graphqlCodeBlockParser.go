1c7333c6cdbf815aca78ae8c0d6407279f38a76c
jansen <liasece@gmail.com>
2022-11-28 14:39:07 +0800

    fix graphql parser

--- src/code/graphqlCodeBlockParser.go
+++ src/code/graphqlCodeBlockParser.go
@@ -8,6 +8,8 @@ var (
 	GraphqlBlockExplain2         CodeBlockType = CodeBlockType{"explain2", true, `(?s)\s*\"([^\n]*)\"\s*$`, 0, 1, -1, nil, "", "", 0, false}
 	GraphqlBlockTypeInput        CodeBlockType = CodeBlockType{"input", true, `(?s)^\s*(extend\s+)?\s*input\s+(\w+)\s*(implements\s+\w+\s*)?\s*(\{\s*(.*?)\s*\})\s*$`, 0, 2, 5, nil, "\n", "{}", 4, false}
 	GraphqlBlockTypeInputField   CodeBlockType = CodeBlockType{"input_field", true, `(?s)^\s*((\w+)(\(([\S\s]+)\))?:\s*([\w!\[\]]+)(\s*=\s*[\w]*)?(\s*@[^\n]*)*)\s*$`, 1, 2, 4, []string{"input"}, "\n|,", "()", 3, true}
+	GraphqlBlockTypeEnum         CodeBlockType = CodeBlockType{"enum", true, `(?s)^\s*((extend\s+)?\s*enum\s+(\w+)\s*(implements\s+\w+\s*)?\s*(\{\s*(.*?)\s*\}))\s*$`, 1, 3, 6, nil, "\n", "{}", 5, false}
+	GraphqlBlockTypeEnumField    CodeBlockType = CodeBlockType{"enum_field", true, `(?s)^\s*((\w+)(\s*@[^\n]*)*)\s*$`, 1, 2, -1, []string{"enum"}, "\n|,", "()", 1, true}
 )
 
 func NewGraphqlCodeBlockParser() *CodeBlockParser {
@@ -20,6 +22,8 @@ func NewGraphqlCodeBlockParser() *CodeBlockParser {
 			GraphqlBlockExplain2,
 			GraphqlBlockTypeInput,
 			GraphqlBlockTypeInputField,
+			GraphqlBlockTypeEnum,
+			GraphqlBlockTypeEnumField,
 		},
 		PairKeys:          []string{"{}", "[]", "()", `""" """`, "\"\""},
 		LineCommentKey:    "#",
